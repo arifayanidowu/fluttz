4633c04222f7355ac9737ae834e3439718bc2f1f
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2021-01-30 18:23:34 +0100

    Modified validateMiddleware.js

--- middleware/validateMiddleware.js
+++ middleware/validateMiddleware.js
@@ -8,6 +8,127 @@ contains - should contain condition value
 export const validateMiddleware = async (req, res, next) => {
   let { data, rule } = req.body;
 
+  //Deep Nested object Check start
+  if (rule.condition === "eq" && rule?.field?.includes(".")) {
+    let arr = rule?.field?.split(".");
+
+    if (!data[arr[0]].hasOwnProperty(arr[1])) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    } else if (data[arr[0]][arr[1]] !== rule.condition_value) {
+      return res.status(400).json({
+        message: `field ${rule.field} failed validation.`,
+        status: "error",
+        data: {
+          validation: {
+            error: true,
+            field: rule.field,
+            field_value: data[rule.field],
+            condition: rule.condition,
+            condition_value: rule.condition_value,
+          },
+        },
+      });
+    } else {
+      return next();
+    }
+  }
+
+  // Deep nested :gt:
+  if (rule.condition === "gt" && rule?.field?.includes(".")) {
+    let arr = rule?.field?.split(".");
+
+    if (!data[arr[0]].hasOwnProperty(arr[1])) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    } else if (data[arr[0]][arr[1]] > rule.condition_value) {
+      return next();
+    } else {
+      return res.status(400).json({
+        message: `field ${rule.field} failed validation.`,
+        status: "error",
+        data: {
+          validation: {
+            error: true,
+            field: rule.field,
+            field_value: data[rule.field],
+            condition: rule.condition,
+            condition_value: rule.condition_value,
+          },
+        },
+      });
+    }
+  }
+
+  // Deep nested :gte:
+  if (rule.condition === "gte" && rule?.field?.includes(".")) {
+    let arr = rule?.field?.split(".");
+
+    if (!data[arr[0]].hasOwnProperty(arr[1])) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    } else if (
+      data[arr[0]][arr[1]] > rule.condition_value ||
+      data[arr[0]][arr[1]] === rule.condition_value
+    ) {
+      return next();
+    } else {
+      return res.status(400).json({
+        message: `field ${rule.field} failed validation.`,
+        status: "error",
+        data: {
+          validation: {
+            error: true,
+            field: rule.field,
+            field_value: data[rule.field],
+            condition: rule.condition,
+            condition_value: rule.condition_value,
+          },
+        },
+      });
+    }
+  }
+
+  // Deep nested :neq:
+  if (rule.condition === "neq" && rule?.field?.includes(".")) {
+    let arr = rule?.field?.split(".");
+
+    if (!data[arr[0]].hasOwnProperty(arr[1])) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    } else if (data[arr[0]][arr[1]] !== rule.condition_value) {
+      return next();
+    } else {
+      return res.status(400).json({
+        message: `field ${rule.field} failed validation.`,
+        status: "error",
+        data: {
+          validation: {
+            error: true,
+            field: rule.field,
+            field_value: data[rule.field],
+            condition: rule.condition,
+            condition_value: rule.condition_value,
+          },
+        },
+      });
+    }
+  }
+
+  // Deep Nested Object check ends
+
   if ((typeof rule || typeof data) !== "object") {
     return res.status(400).json({
       message: "Invalid JSON payload passed.",
@@ -33,16 +154,6 @@ export const validateMiddleware = async (req, res, next) => {
   }
 
   if (typeof data === "object" && !data.hasOwnProperty(rule.field)) {
-    // if (typeof rule === "string" && rule?.includes(".")) {
-    //   let arr = rule?.field?.split(".");
-    //   if (!data[arr[0]].hasOwnProperty(arr[1])) {
-    //     return res.status(400).json({
-    //       message: `field ${rule.field} is missing from data.`,
-    //       status: "error",
-    //       data: null,
-    //     });
-    //   }
-    // }
     return res.status(400).json({
       message: `field ${rule.field} is missing from data.`,
       status: "error",
@@ -81,17 +192,25 @@ export const validateMiddleware = async (req, res, next) => {
         },
       },
     });
+  } else if (
+    typeof data === "object" &&
+    rule.condition === "eq" &&
+    rule.condition_value !== data[rule.field]
+  ) {
+    return res.status(400).json({
+      message: `field ${rule.field} failed validation.`,
+      status: "error",
+      data: {
+        validation: {
+          error: true,
+          field: rule.field,
+          field_value: data[rule.field],
+          condition: rule.condition,
+          condition_value: rule.condition_value,
+        },
+      },
+    });
   }
-  // else if (
-  //   typeof data === "object" &&
-  //   rule.condition === "eq" &&
-  //   rule?.field?.includes(".")
-  // ) {
-  //   let arr = rule?.field?.split(".");
-  //   arr.forEach((item, i) => {
-  //     console.log(data[arr[i]]);
-  //   });
-  // }
 
   if (rule.condition === "gte") {
     if (
@@ -151,6 +270,32 @@ export const validateMiddleware = async (req, res, next) => {
         },
       },
     });
+  }
+
+  if (
+    (rule.condition === "contains" && Array.isArray(data)) ||
+    typeof data === "string"
+  ) {
+    if (
+      typeof data === "string" &&
+      !data?.toLowerCase().includes(rule.field?.toLowerCase())
+    ) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    }
+    if (
+      Array.isArray(data) &&
+      !data.includes(rule.condition_value, rule.field)
+    ) {
+      return res.status(400).json({
+        message: `field ${rule.field} is missing from data.`,
+        status: "error",
+        data: null,
+      });
+    }
   }
 
   next();
